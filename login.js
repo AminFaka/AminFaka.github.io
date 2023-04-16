577fb7c778eb7d5c198c22dced473748011085a8
AminFaka <123004559+AminFaka@users.noreply.github.com>
2023-04-16 15:38:14 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,41 +1,10 @@
-// De client id die je hebt gegeven
-const CLIENT_ID = '434251851653-2t5vuufp0tu4iv2omko3d8pt40podqrv.apps.googleusercontent.com';
+function onSignIn(googleUser) {
+  // Get the Google ID token and Google access token
+  var id_token = googleUser.getAuthResponse().id_token;
+  var access_token = googleUser.getAuthResponse().access_token;
 
-// Laad de Google API-bibliotheek
-function loadGoogleAuth() {
-  gapi.load('auth2', function() {
-    gapi.auth2.init({
-      client_id: CLIENT_ID
-    });
-  });
+  // Send the ID token and access token to your server for verification
+  // In this example, we'll send them to the server as query parameters in the URL
+  var url = "https://aminfaka.github.io/menu.html?id_token=" + id_token + "&access_token=" + access_token;
+  window.location.href = url;
 }
-
-// Inloggen met Google
-function googleLogin() {
-  gapi.auth2.getAuthInstance().signIn().then(
-    function(success) {
-      const user = success.getBasicProfile();
-      const name = user.getName();
-      const email = user.getEmail();
-      const imgURL = user.getImageUrl();
-      const msg = `Welcome ${name}! You are logged in with ${email}.`;
-      document.getElementById('msg-container').innerHTML = msg;
-      console.log(`User logged in with email: ${email}`);
-      // Voeg hier eventuele verdere logica toe, zoals doorverwijzing naar menu.html
-    },
-    function(error) {
-      console.log(`Error logging in: ${error}`);
-    }
-  );
-}
-
-// Voeg event listeners toe aan de knoppen
-window.onload = function() {
-  document.getElementById('login-btn').addEventListener('click', googleLogin);
-  document.getElementById('guest-btn').addEventListener('click', function() {
-    window.location.href = "aminfaka.github.io/menu.html";
-  });
-}
-
-// Laad de Google API-bibliotheek wanneer de pagina wordt geladen
-window.addEventListener('load', loadGoogleAuth);
